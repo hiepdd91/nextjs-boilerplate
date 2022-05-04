d3eda76ace249554a7a625bca7b8d2e1719279c4
Đoàn Đức Hiệp <hiepdd91@gmail.com>
2022-05-04 17:16:27 +0700

    feat: boilerplate started

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  eslint: {
+    dirs: [
+      "src/components",
+      "src/configs",
+      "src/constants",
+      "src/containers",
+      "src/hooks",
+      "src/layouts",
+      "src/models",
+      "src/pages",
+      "src/recoils",
+      "src/utils",
+    ],
+  },
+  experimental: {
+    outputStandalone: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/configs/seo.ts
@@ -0,0 +1,6 @@
+const seoConstants = {
+  defaultTitle: "NextJS Boilerplate",
+  description: "NextJS Boilerplate",
+};
+
+export default seoConstants;
